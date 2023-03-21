e7038728d9eab631d1a0bce7ff14eb11180dd2af
Tazkia Damayanti <tazkiadamayanti@gmail.com>
2023-03-21 12:01:04 +0700

    nyoba

---
+++ Modul2/Modul2.java
@@ -0,0 +1,31 @@
+package Modul2;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Latihan1 {
+    static ArrayList<String> namaHewan = new ArrayList<>(List.of("Sapi","Kelinci", "Kambing", "Unta", "Domba"));
+    static ArrayList<String> deleteHewan = new ArrayList<>(List.of("Kelinci", "Kambing", "Unta"));
+
+//    public void hapusHewan(){
+//        for (String hewan : namaHewan){
+//            System.out.println(hewan);
+//        }
+//    }
+
+    public static void main(String[] args) {
+//        namaHewan.remove(0);
+//        namaHewan.remove(3);
+        System.out.println("Hewan : " + namaHewan);
+        System.out.println("Hewan yang dihapus : ");
+        System.out.println(deleteHewan);
+
+        for (String hapusHewan : deleteHewan){
+            namaHewan.remove(hapusHewan);
+        }
+
+        System.out.println("Output Hewan :");
+        System.out.println(namaHewan);
+    }
+}
